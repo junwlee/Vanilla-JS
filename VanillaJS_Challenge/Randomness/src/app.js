94bda4397e52429cd833781670d385e5816dd097
junwlee <juns971229@gmail.com>
2024-02-29 23:37:22 +0900

    Randomness & linear-gradient

---
+++ VanillaJS_Challenge/Randomness/src/app.js
@@ -0,0 +1,29 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
+
+document.body.style.background = `linear-gradient(to left top, ${colors[Math.floor(Math.random() * colors.length)]}, ${colors[Math.floor(Math.random() * colors.length)]})`;
+
+function paintBg() {
+    document.body.style.background = `linear-gradient(to left top, ${colors[Math.floor(Math.random() * colors.length)]}, ${colors[Math.floor(Math.random() * colors.length)]})`;
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", paintBg);
